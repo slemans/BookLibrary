1e57e1b764f5da9be4df418ff08ce0b191b6acb3
Pavel Bazylevich <sleman@yandex.ru>
2023-04-13 17:06:18 +0300

    Start Project

---
+++ BookLibrary/BookLibrary/Scenses/MainScreen/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  BookLibrary
+//
+//  Created by Павел on 13.04.23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
